081129df086b48c0959741c47f55c1e54e5135b5
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-10-17 15:34:44 -0500

    Added Backend

--- potterycommerce/home/models.py
+++ potterycommerce/home/models.py
@@ -2,6 +2,69 @@ from django.db import models
 
 from wagtail.models import Page
 
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+
+from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
+
+
+class Product(Page):
+    sku = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('sku'),
+        FieldPanel('price'),
+        FieldPanel('image'),
+        FieldPanel('short_description'),
+        InlinePanel('custom_fields', label='Custom fields'),
+    ]
+
+class ProductCustomField(Orderable):
+    product = ParentalKey(Product, on_delete=models.CASCADE, related_name='custom_fields')
+    name = models.CharField(max_length=255)
+    options = models.CharField(max_length=500, null=True, blank=True)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('options')
+    ]
+    def get_context(self, request):
+        context = super().get_context(request)
+        fields = []
+        for f in self.custom_fields.get_object_list():
+            if f.options:
+                f.options_array = f.options.split('|')
+                fields.append(f)
+            else:
+                fields.append(f)
+
+        context['custom_fields'] = fields
+
+        return context
+
 
 class HomePage(Page):
-    pass
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        context['products'] = Product.objects.child_of(self).live()
+
+        return context
+
+@register_setting
+class SnipcartSettings(BaseSiteSetting):
+    api_key = models.CharField(
+        max_length=255,
+        help_text='Your Snipcart public API key'
+    )
