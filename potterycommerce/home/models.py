1a1c0fd90141d7c3d0ed0fe9d0c0f7c24b0b0847
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-10-18 16:58:57 -0500

    Added test products

--- potterycommerce/home/models.py
+++ potterycommerce/home/models.py
@@ -58,9 +58,9 @@ class HomePage(Page):
     def get_context(self, request):
         context = super().get_context(request)
 
-       # context['products'] = Product.objects.child_of(self).live()
-        #products = self.get_children().live()
-        products = ['a','bb']
+        #context['products'] = Product.objects.child_of(self).live()
+        products =  Product.objects.child_of(self.get_descendants()[0]).live()
+        #products = ['a','bb']
         context['products'] = products
         return context
 
