4009b82e25c4607b88da1f2d207617f2ee8c3510
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-02-01 23:24:27 -0500

    Added about

---
+++ potterycommerce/potterycommerce/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def about(request):
+
+    
+    context = {
+        "key": 0,
+    }
+    return render(request, 'home/about.html', context)
+
+def shop(request):
+
+    
+    context = {
+        "key": 0,
+    }
+    return render(request, 'home/shop.html', context)
