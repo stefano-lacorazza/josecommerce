b9aaa3ddb2c17c348c339ce271117c3887942e8c
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-03-21 14:11:37 -0500

    Update base.py

--- potterycommerce/potterycommerce/settings/base.py
+++ potterycommerce/potterycommerce/settings/base.py
@@ -137,19 +137,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
-
-#STATICFILES_DIRS = [
- #   os.path.join(PROJECT_DIR, "static"),
+#STATICFILES_FINDERS = [
+#    "django.contrib.staticfiles.finders.FileSystemFinder",
+#    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 #]
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 #STATIC_URL = "/static/"
