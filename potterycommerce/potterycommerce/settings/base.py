ebf38202dc10c6268bc6d2454d0a325be328b78b
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-03-21 13:52:40 -0500

    Update base.py

--- potterycommerce/potterycommerce/settings/base.py
+++ potterycommerce/potterycommerce/settings/base.py
@@ -157,8 +157,8 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#MEDIA_URL = "/media/"
 
 
 # Wagtail settings
